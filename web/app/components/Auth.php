7410ba1d26d9726906a1c9b6da598f57fe5cad0d
AnzeBlaBla <anzemajblabla@gmail.com>
2023-04-28 15:36:57 +0200

    Auth form change

--- web/app/components/Auth.php
+++ web/app/components/Auth.php
@@ -8,23 +8,40 @@ return function ($h) {
     /**
      * @var \AnzeBlaBla\Framework\Component $this
      */
-    $this->data->view = 'login';
-
-    $switchView = $h->function(function ($view) {
-        $this->data->view = $view;
-    });
-
-    $nextView = $this->data->view == 'login' ? 'register' : 'login';
-
     return <<<HTML
         <div>
             <h2>Auth</h2>
 
-            {$h->if($this->data->view == 'login', $h->component('components/Login'))}
-            {$h->if($this->data->view == 'register', $h->component('components/Register'))}
-            
-            <button onclick="{$switchView}('{$nextView}')">{$nextView}</button>
+            <div id="login_form">
+                {$h->component('components/Login')}
+            </div>
 
+            <div id="register_form" style="display: none">
+                {$h->component('components/Register')}
+            </div>
+
+            <a href="#" onclick="switchAuthForm(); return false;" id="switch">Register</a>
+
+            <script>
+                function switchAuthForm() {
+                    const loginForm = document.getElementById('login_form');
+                    const registerForm = document.getElementById('register_form');
+                    const switchLink = document.getElementById('switch');
+
+                    if (loginForm.style.display === 'none') {
+                        loginForm.style.display = 'block';
+                        registerForm.style.display = 'none';
+                        switchLink.innerText = 'Register';
+                    } else {
+                        loginForm.style.display = 'none';
+                        registerForm.style.display = 'block';
+                        switchLink.innerText = 'Login';
+                    }
+                }
+            </script>
+
+
+            
         </div>
     HTML;
 };
